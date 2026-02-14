f2ad51517039319d6eb1be186a61ee14338e1c12
Luis Sarmiento <48292291+dudunesto@users.noreply.github.com>
2026-02-14 10:44:14 +0000

    Landing-Page-Luis-Samrmiento

--- src/js/components/Cards.jsx
+++ src/js/components/Cards.jsx
@@ -5,10 +5,14 @@ const Cards = () => {
         <div className="cards img-fluid">
             
                 <img src="src/img/rigo-baby.jpg" className="card-img-top" alt="..."></img>
-                <div className="card-body">
+                <div className="card-body ">
+                    <div className="d-flex justify-content-center align-items-center">
                     <h5 className="card-title">Card title</h5>
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                    </div>
+                    <p className="card-text text-center">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                    <div className="d-flex justify-content-center align-items-center">
+                    <a href="#" className="btn btn-primary ">Go somewhere</a>
+                    </div>
                 </div>
             
         </div>
